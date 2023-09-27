dbb6e5ca82e9c8f8bf6b5366f12f3ebc6e38f3e0
JJongmen <pjy90123@gmail.com>
2023-09-27 17:09:05 +0900

    멤버십종류 ENUM 변경 리팩토링

---
+++ src/test/java/com/jyp/tddmembership/MembershipType.java
@@ -0,0 +1,16 @@
+package com.jyp.tddmembership;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MembershipType {
+
+    NAVER("네이버"),
+    LINE("라인"),
+    KAKAO("카카오"),
+    ;
+
+    public final String companyName;
+}
