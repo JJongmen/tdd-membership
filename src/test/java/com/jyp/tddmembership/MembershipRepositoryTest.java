7ae4d316d20bcf66575c457a43fba0078d35bb58
JJongmen <pjy90123@gmail.com>
2023-09-27 16:58:23 +0900

    MemberShipRepository 생성

---
+++ src/test/java/com/jyp/tddmembership/MembershipRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.jyp.tddmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class MembershipRepositoryTest {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Test
+    void MembershipRepository가Null이아님() {
+        assertThat(membershipRepository).isNotNull();
+    }
+}
