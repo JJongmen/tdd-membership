dc2725626b0689a5c6fd10676ba7ef63579c724e
JJongmen <pjy90123@gmail.com>
2023-09-27 17:04:51 +0900

    멤버십등록 테스트 성공

--- src/test/java/com/jyp/tddmembership/MembershipRepositoryTest.java
+++ src/test/java/com/jyp/tddmembership/MembershipRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class MembershipRepositoryTest {
@@ -13,7 +13,21 @@ public class MembershipRepositoryTest {
     private MembershipRepository membershipRepository;
 
     @Test
-    void MembershipRepository가Null이아님() {
-        assertThat(membershipRepository).isNotNull();
+    void 멤버십등록() {
+        // given
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipName("네이버")
+                .point(10000)
+                .build();
+
+        // when
+        final Membership result = membershipRepository.save(membership);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getUserId()).isEqualTo("userId");
+        assertThat(result.getMembershipName()).isEqualTo("네이버");
+        assertThat(result.getPoint()).isEqualTo(10000);
     }
 }
