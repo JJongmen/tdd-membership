543db2828afe2a0e3fcc78828a0d560ed383c91e
JJongmen <pjy90123@gmail.com>
2023-10-01 13:24:27 +0900

    RatePointService: 확장성 고려 리팩터링

---
+++ src/test/java/com/jyp/tddmembership/app/point/PointService.java
@@ -0,0 +1,5 @@
+package com.jyp.tddmembership.app.point;
+
+public interface PointService {
+    int calculateAmount(final int price);
+}
--- src/test/java/com/jyp/tddmembership/app/point/RatePointService.java
+++ src/test/java/com/jyp/tddmembership/app/point/RatePointService.java
@@ -3,10 +3,11 @@ package com.jyp.tddmembership.app.point;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RatePointService {
+public class RatePointService implements PointService {
 
     public static final int POINT_RATE = 1;
 
+    @Override
     public int calculateAmount(final int price) {
         return price * POINT_RATE / 100;
     }
